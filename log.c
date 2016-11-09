94b7e659166777670ccc81addb45b02ce1ccd82c
Sam Izzo <sam@horsedrawngames.com>
2016-11-10 00:43:51 +1100

    Remove log_debug in release.

--- log.c
+++ log.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+#if defined(DEBUG)
 void log_debug(const char* format, ...)
 {
     va_list args;
@@ -9,3 +10,4 @@ void log_debug(const char* format, ...)
     vprintf(format, args);
     va_end(args);
 }
+#endif
--- log.h
+++ log.h
@@ -1,6 +1,10 @@
 #ifndef _log_h
 #define _log_h
 
+#if defined(DEBUG)
 void log_debug(const char* format, ...);
+#else
+#define log_debug(...) do { } while (0)
+#endif
 
 #endif
