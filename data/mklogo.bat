d25644b632df6f0f73d44a66a79a26e2b1a00b54
Sam Izzo <sam@horsedrawngames.com>
2020-01-19 12:21:05 +1100

    Data

---
+++ data/mklogo.bat
@@ -0,0 +1,5 @@
+bin2h -c s2016_0 < s2016_0.tga > s2016_0.h
+bin2h -c s2016_1 < s2016_1.tga > s2016_1.h
+bin2h -c s2016_2 < s2016_2.tga > s2016_2.h
+bin2h -c s2016_3 < s2016_3.tga > s2016_3.h
+bin2h -c s2016_4 < s2016_4.tga > s2016_4.h
